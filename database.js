3a5ccbbb24e0b8abf01659931f23390ce37982ca
gr1mw1k <ameyashinde3@gmail.com>
2023-11-03 14:01:30 +0530

    trial

---
+++ database.js
@@ -0,0 +1,26 @@
+const empTblData = document.getElementById("empTblData");
+
+const url = 'http://localhost:8080/emp/get-user';
+
+async function getEmpData(){
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.length);
+
+    if(data.length != 0){
+        data.forEach((ele) =>{
+            const tr = document.createElement("tr")
+            tr.innerHTML = `<td>${ele.empId}</td>
+            <td>${ele.name}</td>
+            <td>${ele.loginId}</td>
+            <td>${ele.dob}</td>
+            <td>${ele.gender}</td>
+            <td>${ele.address}</td>
+            <td>${ele.city}</td>
+            <td>${ele.state}</td>`;
+            empTblData.appendChild(tr);
+        })
+    }
+}
+
+getEmpData();
